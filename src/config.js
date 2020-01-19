ce61fa4a19156330aa92656b8afedcafd9ff5bcf
Javier Stifano <jstifano18@gmail.com>
2020-01-19 09:52:35 -0400

    Variables globales de la aplicación

---
+++ src/config.js
@@ -0,0 +1,6 @@
+// Variable global de la aplicación
+export const config = {
+    baseUrl: 'http://gateway.marvel.com/v1/public',
+    apiKey: '59d5754151ef7afccb4a648c9beee978',
+    hashMarvelApi: '927f5cbd14c836d3f548b3bf2980aa77'
+}
